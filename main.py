69507ff2696b203eb75a5cb1fbf733aa902659be
Vic8t <vic8.taillieu@gmail.com>
2022-04-15 21:19:19 -0400

    Implement command line interface for main

--- main.py
+++ main.py
@@ -1,36 +1,108 @@
 import logging
 
+import click
 import coloredlogs
 
 from Coach import Coach
-# from connect4.Connect4Game import Connect4Game as Game
-# from connect4.keras.NNet import NNetWrapper as nn
-from tictactoe.keras.NNet import NNetWrapper as nn
-from tictactoe.TicTacToeGame import TicTacToeGame as Game
+from connect4.Connect4Game import Connect4Game
+from connect4.keras.NNet import NNetWrapper as Connect4NNet
+from tictactoe.TicTacToeGame import TicTacToeGame
+from tictactoe.keras.NNet import NNetWrapper as TicTacToeNNet
 from utils import dotdict
 
 log = logging.getLogger(__name__)
 
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
-args = dotdict({
-    'numIters': 3,
-    'numEps': 25,             # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 15,      #
-    'updateThreshold': 0.6,   # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'maxlenOfQueue': 200000,  # Number of game examples to train the neural networks.
-    'numMCTSSims': 25,        # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,       # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1,
 
-    'checkpoint': './temp/',
-    'load_model': False,
-    'load_folder_file': ('./checkpoints', 'best.h5'),
-    'numItersForTrainExamplesHistory': 20
-})
+@click.command()
+@click.argument('game', type=click.Choice(['tictactoe', 'connect4']))
+@click.option(
+    '-iters', '--iterations',
+    default=3, show_default=True,
+    help='Number of iterations'
+)
+@click.option(
+    '-eps', '--episodes',
+    default=25, show_default=True,
+    help='Number of self-play games per iteration'
+)
+@click.option(
+    '-mcts', '--mcts-simulations',
+    default=25, show_default=True,
+    help='Number of MCTS simulations per turn'
+)
+@click.option(
+    '--checkpoint',
+    type=click.Path(file_okay=False),
+    default='./temp/',
+    help='Path to save checkpoints'
+)
+@click.option(
+    '--load-model',
+    is_flag=True,
+    help='Load model from checkpoint'
+)
+@click.option(
+    '--load-model-file',
+    type=(str, str),
+    default=('./checkpoints', 'checkpoint.h5'),
+    help='Folder and file name of model to load'
+)
+@click.option(
+    '--temperature-threshold',
+    default=15, show_default=True,
+    help='Number of moves in an episode before the temperature changes from 1 to 0'
+)
+@click.option(
+    '--update-threshold',
+    default=0.6, show_default=True,
+    help='Minimal win rate to accept new network'
+)
+@click.option(
+    '--cpuct',
+    default=1, show_default=True,
+    help='Level of exploration'
+)
+@click.option(
+    '--games-compare',
+    default=40, show_default=True,
+    help='Number of games to play to determine if new network is accepted'
+)
+@click.option(
+    '--iters-for-train',
+    default=20, show_default=True,
+    help='Number of iterations used for training'
+)
+@click.option(
+    '--max-queue-length',
+    default=200000, show_default=True,
+    help='Maximum number of game examples for training'
+)
+def main(game, iterations, episodes, mcts_simulations, checkpoint, load_model, load_model_file, temperature_threshold,
+         update_threshold, cpuct, games_compare, iters_for_train, max_queue_length):
+    args = dotdict({
+        'numIters': iterations,
+        'numEps': episodes,
+        'numMCTSSims': mcts_simulations,
+        'checkpoint': checkpoint,
+        'load_model': load_model,
+        'load_folder_file': load_model_file,
+        'tempThreshold': temperature_threshold,
+        'updateThreshold': update_threshold,
+        'cpuct': cpuct,
+        'arenaCompare': games_compare,
+        'numItersForTrainExamplesHistory': iters_for_train,
+        'maxlenOfQueue': max_queue_length
+    })
 
+    if game == "tictactoe":
+        Game = TicTacToeGame
+        nn = TicTacToeNNet
+    elif game == "connect4":
+        Game = Connect4Game
+        nn = Connect4NNet
 
-def main():
     log.info('Loading %s...', Game.__name__)
     g = Game()
 
